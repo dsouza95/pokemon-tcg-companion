40779ec5677b1aeeb396fb6e9d4bda185fc9fac6
Daniel Souza <dsouza1995@gmail.com>
2026-02-23 13:43:36 -0300

    update fly configuration and env variables

--- backend/pokemon-tcg-companion/core/settings.py
+++ backend/pokemon-tcg-companion/core/settings.py
@@ -7,7 +7,6 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     allowed_origins: str = ""
-    backend_url: str = "http://localhost:8000"
     clerk_secret_key: str = ""
     clerk_authorized_party: str = "http://localhost:3000"
     database_url: str = "sqlite+aiosqlite:///./test.db"
